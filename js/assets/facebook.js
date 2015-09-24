c8aadeef285c258a38d94f0dbfc70a572693d915
JeppeSigaard <jeppe@smatmonkey.dk>
2015-09-24 14:59:34 +0200

    ny facebook kode

--- js/assets/facebook.js
+++ js/assets/facebook.js
@@ -1,14 +1,15 @@
-
-$(function() {
-  $.ajaxSetup({ cache: true });
-  $.getScript('//connect.facebook.net/da_DK/sdk.js', function(){
+window.fbAsyncInit = function() {
     FB.init({
-      appId: '1500566816925880',
-      version: 'v2.3' // or v2.0, v2.1, v2.0
-    });     
-    
-    if (typeof facebookInit === 'function'){
-        facebookInit();
-    }
-  });
-});
+      appId      : '1500566816925880',
+      xfbml      : true,
+      version    : 'v2.4'
+    });
+  };
+
+(function(d, s, id){
+ var js, fjs = d.getElementsByTagName(s)[0];
+ if (d.getElementById(id)) {return;}
+ js = d.createElement(s); js.id = id;
+ js.src = "//connect.facebook.net/en_US/sdk.js";
+ fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
