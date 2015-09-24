f96d782bf05cbd6b5cc60d81989c5cca31125ca2
JeppeSigaard <jeppe@smatmonkey.dk>
2015-09-24 14:35:15 +0200

    setting up js for facebook and adding no-space rule to posts inner

---
+++ js/assets/facebook.js
@@ -0,0 +1,13 @@
+$(function() {
+  $.ajaxSetup({ cache: true });
+  $.getScript('//connect.facebook.net/da_DK/sdk.js', function(){
+    FB.init({
+      appId: '{your-app-id}',
+      version: 'v2.3' // or v2.0, v2.1, v2.0
+    });     
+    
+    if (typeof facebookInit === 'function'){
+        facebookInit();
+    }
+  });
+});
--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// @koala-append 'assets/facebook.js';
 // @koala-append 'modules/header.js';
 // @koala-append 'modules/toggle-fixed.js';
 // @koala-append 'modules/sync-scroll.js';
