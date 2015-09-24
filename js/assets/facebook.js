6caf5b0797d11b1439561b2e3bf7b957af18c2ea
JeppeSigaard <jeppe@smatmonkey.dk>
2015-09-24 14:41:45 +0200

    facebook app id

--- js/assets/facebook.js
+++ js/assets/facebook.js
@@ -3,7 +3,7 @@ $(function() {
   $.ajaxSetup({ cache: true });
   $.getScript('//connect.facebook.net/da_DK/sdk.js', function(){
     FB.init({
-      appId: '{your-app-id}',
+      appId: '1500566816925880',
       version: 'v2.3' // or v2.0, v2.1, v2.0
     });     
     
