0fa66274cab35bdff2913a38dfb798bb5e462509
JeppeSigaard <jeppe@smatmonkey.dk>
2015-09-24 14:39:52 +0200

    resolving merges

--- js/assets/facebook.js
+++ js/assets/facebook.js
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 $(function() {
   $.ajaxSetup({ cache: true });
   $.getScript('//connect.facebook.net/da_DK/sdk.js', function(){
@@ -13,17 +12,3 @@ $(function() {
     }
   });
 });
-=======
-=======
->>>>>>> origin/master
-(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/da_DK/sdk.js#xfbml=1&version=v2.4&appId=1566523690226833";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
-<<<<<<< HEAD
->>>>>>> origin/master
-=======
->>>>>>> origin/master
